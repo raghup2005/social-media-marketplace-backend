00b95ff6056b44603d38d817d42b1b35c17d2f30
raghu narayana <raghupolisetty5@gmail.com>
2026-04-13 01:08:24 +0530

    committed emailstr

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 from sqlmodel import SQLModel,Field
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from typing import Optional
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    email: str = Field(unique=True)
+    email: EmailStr = Field(unique=True)
     hashed_password: str
     
     
